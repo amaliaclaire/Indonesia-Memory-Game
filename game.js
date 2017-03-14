654332cff70d21c45f7073e3a525e2d4f8e940ea
Amalia Claire Fergin <acfergin@msn.com>
2017-03-14 12:49:15 -0700

    Memory Game Saya Ingat

--- game.js
+++ game.js
@@ -1,18 +1,13 @@
 //jQuery adding class, img src and img id
 var addBarongCards;
-
-
-
 //Where the images lie
 var URL = "https://pixabay.com/api//?key=3524767-02f5ba794561ee4931dcf448b\&q=indonesia\&image_type=photo"
-
 var indoPicsArray = [];
-var shuffledIndoArray; //used in the global scope
+var shuffledIndoArray = []; //used in the global scope
 
 
 //fetching the image URLs
 $.getJSON(URL, function(data){
-
   if(parseInt(data.totalHits) > 0) { //parseInt changes a string into a number. if x = "9000", parseInt(x) = 9000
     $.each(data.hits, addURLToArray);  //.each() is a jQuery function that iterate over a collection (array or Object) --> but we are using it over an Array
 
@@ -21,49 +16,37 @@ $.getJSON(URL, function(data){
   } else {
     console.log('no images')
   }
-
-  console.log(data);
 });
-
 
 function addURLToArray(index, hit){
   indoPicsArray.push(hit.webformatURL) // this function will run for every item in data.hits ASK INSTRUCTOR HOW DATA.HITS AND hit is connected
 }
 
 function displaySixIndoImages(){
+  let myDivCardContainer = $('.card-container');
 
   for (var i = 1; i <= 6; i++) {
+    let currentImageURL = shuffledIndoArray[i];
+    let card = $('<div class="card"></div>');
+    let front = $('<div class="front invisible"></div>');
+    let frontImg = $('<img class="frontImg" src="barong-image-cut.png" />');
+    let back = $('<div class="back invisible"></div>');
+    let backImg = $(`<img class="cardBackImage" data-image-id="${i}"  src="${currentImageURL}" />`);
 
-    myDivCardContainer = $(`
-    <div class="card">
-      <div class="front">
-        <img src="barong-image-cut.png">
-      </div>
-      <div class="back">
-        <img id="indoImage${i}" src="">
-      </div>
-    </div>`)
+    front.append(frontImg);
+    back.append(backImg);
+    card.append(front, back);
 
-    $('.card-container').append(myDivCardContainer)
+    myDivCardContainer.append(card);
 
-    //Get an imageId, so that we can get an image element
-    var imageId = "indoImage" + i;
+    $(`.frontImg`).click(function (e){
+      e.stopImmediatePropagation();
+      frontImg.hide()
+      // backImg.show()
+      back.toggleClass('invisible')
 
-    // Get image element, using imageId
-    var imageElement = document.getElementById(imageId)// this is going through the 1st - 6th image
-    // Get a URL for a indonesian picture
-    var currentImageURL = shuffledIndoArray[i]; // RANDOM IMAGES
-    // Assign URL to the src attribute of image element
-    imageElement.src = currentImageURL; // Using the DOM --> we can manipulate it like an object. we have selected it by ID
-
-console.log('hello', $(`#indoImage${i}`));
-    $(`#indoImage${i}`).click(function (){
-      console.log('hisdifjosjdf');
-      alert("handler for .click is called");
 
     });
-
-
   }
 }
 
